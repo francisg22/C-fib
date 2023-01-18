01fab5ad42e8990ed6754d276ef6953d2ce5c00c
francisg <109164838+francisg22@users.noreply.github.com>
2023-01-18 18:09:27 +0000

    working fib

--- .ccls-cache/@home@runner@C-fib/main.c
+++ .ccls-cache/@home@runner@C-fib/main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  int a;
+  int b;
+  int num;
+  int fn;
+  int n;
+  num = 5;
+  a = 0;
+  b = 1;
+  if (num == 1) {
+    printf("%d",0);
+}
+  else if (num == 2){
+    printf("%d",1);
+  }
+  else{
+    fn = 0;
+    n = num-1;
+    for(int i = 0; i < n-1; ++i)
+    {
+      fn = a + b;
+      a = b;
+      b = fn;
+    }
+  }
+  
+  printf("%d",fn);
   return 0;
 }
